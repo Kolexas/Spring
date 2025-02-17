817c471a42c04522eae43c11e54ed06cea365b7c
Kolexas <maksimilyan.klimentev39@gmail.ru>
2025-02-17 19:00:43 +0300

    Dependencies and exceptions in Spring Boot: initial commit.

--- src/main/java/org/skypro/skyshop/exceptions/NoSuchProductException.java
+++ src/main/java/org/skypro/skyshop/exceptions/NoSuchProductException.java
@@ -4,9 +4,4 @@ public class NoSuchProductException extends RuntimeException {
 
     public NoSuchProductException() {
     }
-
-    public NoSuchProductException(Throwable cause) {
-        super(cause);
-    }
-
 }
