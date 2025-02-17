aaa6088bb7e32dde589933f4fd4f6ce6b5ea5f34
Kolexas <maksimilyan.klimentev39@gmail.ru>
2025-02-17 18:59:47 +0300

    Dependencies and exceptions in Spring Boot: initial commit.

---
+++ src/main/java/org/skypro/skyshop/exceptions/NoSuchProductException.java
@@ -0,0 +1,12 @@
+package org.skypro.skyshop.exceptions;
+
+public class NoSuchProductException extends RuntimeException {
+
+    public NoSuchProductException() {
+    }
+
+    public NoSuchProductException(Throwable cause) {
+        super(cause);
+    }
+
+}
--- src/main/java/org/skypro/skyshop/service/BasketService.java
+++ src/main/java/org/skypro/skyshop/service/BasketService.java
@@ -1,5 +1,6 @@
 package org.skypro.skyshop.service;
 
+import org.skypro.skyshop.exceptions.NoSuchProductException;
 import org.skypro.skyshop.model.basket.BasketItem;
 import org.skypro.skyshop.model.basket.ProductBasket;
 import org.skypro.skyshop.model.basket.UserBasket;
@@ -23,7 +24,7 @@ public class BasketService {
 
     public void addProduct(UUID id) {
         if (storageService.getProductById(id).isEmpty()) {
-            throw new IllegalArgumentException(" Такого продукта нет");
+            throw new NoSuchProductException();
         }
         basket.addProduct(id);
     }
